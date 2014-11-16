97096759cd75f2d1e5ea36977b105c632cd01025
Tom Spencer <fiznool@gmail.com>
2014-11-16 09:13:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Passport OAuth 2.0 Refresh
+
+An add-on to the [Passport](http://passportjs.org) authentication library to provide a simple way to refresh your OAuth 2.0 access tokens.
+
+## Installation
+
+```
+npm install passport-oauth2-refresh
+```
+
+## Usage
+
+When setting up your passport strategies, add a call to `refresh.use()` after `passport.use()`.
+
+An example, using the Facebook strategy:
+
+``` js
+var passport = require('passport'),
+  , refresh = require('passport-oauth2-refresh')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+var strategy = new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "http://www.example.com/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  // Make sure you store the refreshToken somewhere!
+  User.findOrCreate(..., function(err, user) {
+    if (err) { return done(err); }
+    done(null, user);
+  });
+});
+
+passport.use(strategy);
+refresh.use(strategy);
+```
+
+When you need to refresh the access token, call `requestNewAccessToken()`:
+
+```
+var refresh = require('passport-oauth2-refresh');
+refresh.requestNewAccessToken('facebook', 'some_refresh_token', function(err, accessToken, refreshToken) {
+  // You have a new access token, store it in the user object,
+  // or use it to make a new request.
+  // `refreshToken` may or may not exist, depending on the strategy you are using.
+  // You probably don't need it anyway, as according to the OAuth 2.0 spec,
+  // it should be the same as the initial refresh token.
+
+});
+
+```
+
+## Why?
+
+Passport is a library which doesn't deal in implementation-specific details. From the author:
+
+> Passport is a library for authenticating requests, and only that. It is not going to get involved in anything that is specific to OAuth, or any other authorization protocol.
+
+Fair enough. Hence, this add-on was born as a way to help deal with refreshing OAuth 2.0 tokens.
+
+It is particularly useful when dealing with Google's OAuth 2.0 implementation, which expires access tokens after 1 hour.
+
+## License
+
+MIT
---
+++ test/refresh.spec.js
@@ -0,0 +1,107 @@
+'use strict';
+
+require('mocha');
+
+var chai = require('chai'),
+    sinon = require('sinon'),
+    expect = chai.expect,
+    AuthTokenRefresh = require('../lib/refresh.js');
+
+chai.use(require('sinon-chai'));
+
+describe('Auth token refresh', function() {
+
+  beforeEach(function() {
+    AuthTokenRefresh._strategies = {};
+  });
+
+  describe('use', function() {
+    it('should add a strategy', function() {
+      var strategy = {
+        name: 'test_strategy',
+        _oauth2: {}
+      };
+
+      AuthTokenRefresh.use(strategy);
+      expect(AuthTokenRefresh._strategies.test_strategy).to.equal(strategy);
+    });
+
+    it('should not add a null strategy', function() {
+      var strategy = null;
+      var fn = function() {
+        AuthTokenRefresh.use(strategy);
+      };
+
+      expect(fn).to.throw(Error, 'Cannot register: strategy is null');
+    });
+
+    it('should not add a strategy with no name', function() {
+      var strategy = {
+        name: '',
+        _oauth2: {}
+      };
+
+      var fn = function() {
+        AuthTokenRefresh.use(strategy);
+      };
+
+      expect(fn).to.throw(Error, 'Cannot register: strategy has no name');
+    });
+
+    it('should not add a non-OAuth 2.0 strategy', function() {
+      var strategy = {
+        name: 'test_strategy',
+        _oauth2: null
+      };
+
+      var fn = function() {
+        AuthTokenRefresh.use(strategy);
+      };
+
+      expect(fn).to.throw(Error, 'Cannot register: strategy test_strategy is not an OAuth2 strategy');
+    });
+  });
+
+  describe('has', function() {
+    it('should return true if a strategy has been added', function() {
+      var strategy = {
+        name: 'test_strategy',
+        _oauth2: {}
+      };
+
+      AuthTokenRefresh.use(strategy);
+      expect(AuthTokenRefresh.has('test_strategy')).to.be.true;
+    });
+
+    it('should return false if a strategy has not been added', function() {
+      expect(AuthTokenRefresh.has('test_strategy')).to.be.false;
+    });
+  });
+
+  describe('request new access token', function() {
+    it('should refresh an access token', function() {
+      var getOAuthAccessTokenSpy = sinon.spy();
+      var strategy = {
+        name: 'test_strategy',
+        _oauth2: {
+          getOAuthAccessToken: getOAuthAccessTokenSpy
+        }
+      };
+      var done = sinon.spy();
+
+      AuthTokenRefresh.use(strategy);
+      AuthTokenRefresh.requestNewAccessToken('test_strategy', 'refresh_token', done);
+
+      expect(getOAuthAccessTokenSpy).to.have.been.calledWith('refresh_token', { grant_type: 'refresh_token' }, done);
+    });
+
+    it('should not refresh if the strategy was not previously registered', function() {
+      var done = sinon.spy();
+      var expectedErr = new Error('Strategy was not registered to refresh a token');
+
+      AuthTokenRefresh.requestNewAccessToken('test_strategy', 'refresh_token', done);
+
+      expect(done).to.have.been.calledWith(expectedErr);
+    });
+  });
+});
